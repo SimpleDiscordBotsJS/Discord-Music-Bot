f7c5a09228c5b1a229337f33d241ab7e6c6bd717
Leonus <26181597+LeonusDH@users.noreply.github.com>
2021-12-26 00:42:27 +0300

    upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js"); //Библиотека
+const client = new Discord.Client({ intents: 32767 }); //Создание клиента
+const config = require("./config.json"); //Загрузка конфига
+
+client.commands = new Discord.Collection();
+
+//===========================================================
+
+const { DisTube } = require("distube");
+const { SpotifyPlugin } = require("@distube/spotify");
+
+client.distube = new DisTube(client, {
+    emitNewSongOnly: true,
+    leaveOnFinish: true,
+    emitAddSongWhenCreatingQueue: false,
+    plugins: [new SpotifyPlugin()]
+});
+module.exports = client;
+
+//===========================================================
+
+require("./Handlers/Events")(client);
+require("./Handlers/Commands")(client);
+
+//===========================================================
+
+//Подключение к боту и вывод ошибки в случае отсутствия токена, или если токен неверен.
+client.login(config.BOT_TOKEN).catch(() => Console.error("[BOT] Invalid Bot Login Token."));
